780efafdb1d0d08ec90140ca690578aad9e18a35
Lalatenduswain <lkswain_2003@yahoo.com>
2020-03-26 00:01:45 +0530

    CommitFromMac

--- HTML/index.php
+++ HTML/index.php
@@ -1,11 +1,12 @@
 <html>
-<body>
+    <head>
+        <title>Home</title>
+    </head>
+        <body>
+            <p>
+            <center>
+                Hello
+            </center>
+            </P>
+        </body>
 
-<h1>My first PHP page</h1>
-
-<?php
-echo "Hello World!";
-?>
-
-</body>
-</html>
